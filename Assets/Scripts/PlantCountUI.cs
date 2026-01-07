8cbb66c4129b4186cc90870e8b5a775f2bd34d3f
EW1028 <eqxwei10@gmail.com>
2026-01-06 22:57:33 -0800

    update

--- Assets/Scripts/PlantCountUI.cs
+++ Assets/Scripts/PlantCountUI.cs
@@ -1,13 +1,17 @@
 using UnityEngine;
 using TMPro;
 
+
 public class PlantCountUI : MonoBehaviour
 {
+    public int seedsLeft;
+    public int seedsPlanted;
     [SerializeField] private TMP_Text _plantedText;
     [SerializeField] private TMP_Text _remainingText;
 
     public void UpdateSeeds (int seedsLeft, int seedsPlanted)
     {
-        
-    }
+        _remainingText.text = seedsLeft.ToString();
+        _plantedText.text = seedsPlanted.ToString();
+    } 
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -11,18 +11,39 @@ public class Player : MonoBehaviour
     private int _numSeedsLeft;
     private int _numSeedsPlanted;
 
+     //private void UpdateSeedCount(int seedsLeft)
+    //{
+     //   _plantCountUI.UpdateSeedCount(_numSeedsLeft, _numSeedsPlanted);
+    //}
+
     private void Start ()
     {
-        
+        _numSeedsLeft = _numSeeds;
+        _numSeedsPlanted = 0;
+        _plantCountUI.UpdateSeeds(_numSeedsLeft, _numSeedsPlanted);
     }
 
     private void Update()
     {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(horizontalInput, verticalInput, 0) * _speed * Time.deltaTime;
+        _playerTransform.Translate(movement, Space.World);
+
+        if (Input.GetKeyDown(KeyCode.Space) && _numSeedsLeft > 0)
+        {
+            PlantSeed();
+            _numSeedsLeft--;
+            _numSeedsPlanted++;
+            _plantCountUI.UpdateSeeds(_numSeedsLeft, _numSeedsPlanted);
+        }
         
     }
 
     public void PlantSeed ()
     {
+        Instantiate(_plantPrefab, _playerTransform.position + _playerTransform.forward * 2, Quaternion.identity);
         
     }
 }
